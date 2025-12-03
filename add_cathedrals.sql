4f8e69cde2b3f89239c2b8876e485187419a0934
LolKIa <remeyztoor@gmail.com>
2025-12-03 18:38:24 +0900

    add cathedrals

---
+++ add_cathedrals.sql
@@ -0,0 +1,30 @@
+CREATE TABLE cathedrals (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(1024) NOT NULL,
+    chief VARCHAR(512) NOT NULL
+);
+
+ALTER TABLE groups
+ADD COLUMN cathedral_id INT;
+
+ALTER TABLE groups
+ADD FOREIGN KEY (cathedral_id) REFERENCES cathedrals(id);
+
+INSERT INTO cathedrals (name, chief)
+VALUES
+('Кафедра информационных технологий', 'Васильева Наталья Васильевна'),
+('Математическая экономика и прикладная информатика', 'Матвеева Нюргуяна Николаевна');
+
+SELECT * FROM cathedrals;
+
+UPDATE groups
+SET cathedral_id = 1
+WHERE short_name IN ('Б-ИВТ-25-1', 'Б-ИВТ-25-2', 'Б-ФИИТ-25');
+
+UPDATE groups
+SET cathedral_id = 2
+WHERE short_name = 'Б-ПИ-25-1';
+
+SELECT *
+FROM groups
+WHERE cathedral_id = 1;
